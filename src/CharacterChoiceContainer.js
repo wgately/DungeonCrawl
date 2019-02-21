31650718e1d78fd432eea0578c5f125ea0943123
Will <w.gately.ii@gmail.com>
2019-02-20 21:55:22 -0800

    character container page

---
+++ src/CharacterChoiceContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class CharacterChoiceContainer extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Character Choices</h1>
+            </div>
+        )
+    }
+
+}
+export default CharacterChoiceContainer;   
